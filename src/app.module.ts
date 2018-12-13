f78bfca459c24dd3f06e8f9a2d695d5cb3b0a6b5
Dragos Strainu <str.dr4605@gmail.com>
2018-12-13 15:20:56 +0200

    delete unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbService } from './db/db.service';
-import { BooksController } from './books/books.controller';
-import { BooksService } from './books/books/books.service';
 import { BooksModule } from './books/books.module';
 import { AuthorsModule } from './authors/authors.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
